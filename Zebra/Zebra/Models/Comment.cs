8f00e3a6ce706cbfb6f5f81f2542262340634602
Linkliman <charlelie.morineau@gmail.com>
2017-03-23 17:50:10 +0100

    Correction Models

---
+++ Zebra/Zebra/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zebra.Models
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+        public int ID_User { get; set; }
+        public Album ID_Album { get; set; }
+        public Music ID_Music { get; set; }
+        public string Content { get; set; }
+    }
+}
