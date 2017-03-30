749ee167e00ad8083eca2d7d72824b39515afa50
Linkliman <charlelie.morineau@gmail.com>
2017-03-30 16:47:36 +0200

    CSS

--- Zebra/Zebra/App_Start/BundleConfig.cs
+++ Zebra/Zebra/App_Start/BundleConfig.cs
@@ -23,9 +23,9 @@ namespace Zebra
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css/css").Include(
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/site.css"));
         }
     }
 }
