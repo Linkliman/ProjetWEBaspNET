dfe6f1bf74c45c5666f92e80f9090fb04dc85a93
Linkliman <charlelie.morineau@gmail.com>
2017-04-06 10:41:38 +0200

    Controlleurs et vues albums et orders

---
+++ Zebra/Zebra/Controllers/AlbumsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Zebra.Models;
+
+namespace Zebra.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Albums
+        public ActionResult Index()
+        {
+            return View(db.Albums.ToList());
+        }
+
+        // GET: Albums/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlbumModels albumModels = db.Albums.Find(id);
+            if (albumModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(albumModels);
+        }
+
+        // GET: Albums/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Albums/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,prix,Note,ID_User")] AlbumModels albumModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Albums.Add(albumModels);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(albumModels);
+        }
+
+        // GET: Albums/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlbumModels albumModels = db.Albums.Find(id);
+            if (albumModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(albumModels);
+        }
+
+        // POST: Albums/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,prix,Note,ID_User")] AlbumModels albumModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(albumModels).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(albumModels);
+        }
+
+        // GET: Albums/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlbumModels albumModels = db.Albums.Find(id);
+            if (albumModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(albumModels);
+        }
+
+        // POST: Albums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AlbumModels albumModels = db.Albums.Find(id);
+            db.Albums.Remove(albumModels);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Zebra/Zebra/Controllers/OrdersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Zebra.Models;
+
+namespace Zebra.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return View(db.Orders.ToList());
+        }
+
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderModels orderModels = db.Orders.Find(id);
+            if (orderModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderModels);
+        }
+
+        // GET: Orders/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Orders/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,ID_User")] OrderModels orderModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(orderModels);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(orderModels);
+        }
+
+        // GET: Orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderModels orderModels = db.Orders.Find(id);
+            if (orderModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderModels);
+        }
+
+        // POST: Orders/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,ID_User")] OrderModels orderModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(orderModels).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(orderModels);
+        }
+
+        // GET: Orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderModels orderModels = db.Orders.Find(id);
+            if (orderModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderModels);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            OrderModels orderModels = db.Orders.Find(id);
+            db.Orders.Remove(orderModels);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
