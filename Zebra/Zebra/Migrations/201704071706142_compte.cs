5477e79560c8e8d16b6b6c2fb45439da72130e8d
lebrun2018 <magdeleine.lebrun@gmail.com>
2017-04-07 19:29:18 +0200

    Compte user OK

---
+++ Zebra/Zebra/Migrations/201704071706142_compte.cs
@@ -0,0 +1,16 @@
+namespace Zebra.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class compte : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
